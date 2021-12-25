9c39c2eaa7a15b4277371843f119bf9b49ad2e69
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-25 05:40:49 +0100

    Add security groups to modules

---
+++ terraform/modules/oracle-arm-instance-kubernetes/security-group.tf
@@ -0,0 +1,45 @@
+resource "oci_core_network_security_group" "oaik_kube" {
+  compartment_id = var.compartment_id
+  vcn_id         = var.vcn_id
+  display_name   = "${var.clustername} kubernetes api security group"
+}
+
+resource "oci_core_network_security_group_security_rule" "oaik_kube" {
+  network_security_group_id = oci_core_network_security_group.oaik_kube.id
+
+  description = "KUBE"
+  direction   = "INGRESS"
+  protocol    = 6
+  source_type = "CIDR_BLOCK"
+  source      = "0.0.0.0/0"
+
+  tcp_options {
+    destination_port_range {
+      min = 6443
+      max = 6443
+    }
+  }
+}
+
+resource "oci_core_network_security_group" "oaik_etcd" {
+  compartment_id = var.compartment_id
+  vcn_id         = var.vcn_id
+  display_name   = "${var.clustername} etcd api security group"
+}
+
+resource "oci_core_network_security_group_security_rule" "oaik_etcd" {
+  network_security_group_id = oci_core_network_security_group.oaik_etcd.id
+
+  description = "ETCD"
+  direction   = "INGRESS"
+  protocol    = 6
+  source_type = "CIDR_BLOCK"
+  source      = "0.0.0.0/0"
+
+  tcp_options {
+    destination_port_range {
+      min = 2379
+      max = 2379
+    }
+  }
+}
--- terraform/modules/oracle-arm-instance-kubernetes/security-list.tf
+++ terraform/modules/oracle-arm-instance-kubernetes/security-list.tf
@@ -1,7 +1,7 @@
 resource "oci_core_security_list" "oaik" {
   compartment_id = var.compartment_id
   vcn_id         = var.vcn_id
-  display_name   = "${var.clustername}-serurity-list"
+  display_name   = "${var.clustername}-security-list"
   egress_security_rules {
     protocol    = "all"
     destination = "0.0.0.0/0"
