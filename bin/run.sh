51ab217dc28c881e3567f1a4125fad607537cd4b
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-25 14:41:53 +0100

    update run deployment script

--- bin/run.sh
+++ bin/run.sh
@@ -5,6 +5,11 @@ cd $GIT_ROOT
 # Setup secrets
 # ./vault.sh
 
+# Run terraform apply
+cd $GIT_ROOT/terraform
+terraform apply -var-file="variables.tfvars"
+cd $GIT_ROOT
+
 # Run kubespray deployment
 docker run --rm -it \
   --mount type=bind,source="$GIT_ROOT"/kubespray/inventory/zelos,dst=/inventory \
@@ -12,5 +17,5 @@ docker run --rm -it \
   quay.io/kubespray/kubespray:v2.17.1 bash -c "ansible-playbook -i /inventory/inventory.ini -b --private-key /root/.ssh/id_rsa cluster.yml"
 
 # Push kubernetes admin config to vault
-# admin_conf="$(cat kubespray/inventory/zelos/artifacts/admin.conf | base64)"
+admin_conf="$(cat kubespray/inventory/zelos/artifacts/admin.conf | base64)"
 # vault kv put CICD/repo/zelos-bootstrap/live/kube-secret admin_conf=$admin_conf
