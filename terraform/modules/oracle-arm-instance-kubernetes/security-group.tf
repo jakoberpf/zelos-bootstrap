846c5bc0d70b314e877883fc449f9a453d0a0493
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-25 15:17:17 +0100

    fix order

--- terraform/modules/oracle-arm-instance-kubernetes/security-group.tf
+++ terraform/modules/oracle-arm-instance-kubernetes/security-group.tf
@@ -1,26 +1,3 @@
-resource "oci_core_network_security_group" "oaik_kube" {
-  compartment_id = var.compartment_id
-  vcn_id         = var.vcn_id
-  display_name   = "${var.clustername}-etcd-security-group-${random_string.deployment_id.result}"
-}
-
-resource "oci_core_network_security_group_security_rule" "oaik_kube" {
-  network_security_group_id = oci_core_network_security_group.oaik_kube.id
-
-  description = "KUBE"
-  direction   = "INGRESS"
-  protocol    = 6
-  source_type = "CIDR_BLOCK"
-  source      = "0.0.0.0/0"
-
-  tcp_options {
-    destination_port_range {
-      min = 6443
-      max = 6443
-    }
-  }
-}
-
 resource "oci_core_network_security_group" "oaik_etcd" {
   compartment_id = var.compartment_id
   vcn_id         = var.vcn_id
@@ -40,6 +17,29 @@ resource "oci_core_network_security_group_security_rule" "oaik_etcd" {
     destination_port_range {
       min = 2379
       max = 2379
+    }
+  }
+}
+
+resource "oci_core_network_security_group" "oaik_kube" {
+  compartment_id = var.compartment_id
+  vcn_id         = var.vcn_id
+  display_name   = "${var.clustername}-kube-security-group-${random_string.deployment_id.result}"
+}
+
+resource "oci_core_network_security_group_security_rule" "oaik_kube" {
+  network_security_group_id = oci_core_network_security_group.oaik_kube.id
+
+  description = "KUBE"
+  direction   = "INGRESS"
+  protocol    = 6
+  source_type = "CIDR_BLOCK"
+  source      = "0.0.0.0/0"
+
+  tcp_options {
+    destination_port_range {
+      min = 6443
+      max = 6443
     }
   }
 }
