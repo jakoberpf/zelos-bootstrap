44502db58092e328a9d8fd434a14c179b093d085
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-29 18:37:38 +0100

    refine terraform deploy script with looping

--- bin/terraform.sh
+++ bin/terraform.sh
@@ -5,5 +5,17 @@ cd $GIT_ROOT
 # Run terraform apply
 cd $GIT_ROOT/terraform
 terraform init -upgrade
-terraform apply -var-file="variables.tfvars"
+
+if [[ $* == *--loop* ]]; then
+  start=`date +%s`
+  until terraform apply -var-file="variables.tfvars" -auto-approve
+  do
+    now=`date +%s`
+    runtime=$((now-start))
+    echo "Error during apply. OCI Hosts possibly out of capacity. Try again... (running for $runtime)"
+  done
+else
+  terraform apply -var-file="variables.tfvars"
+fi
+
 cd $GIT_ROOT
