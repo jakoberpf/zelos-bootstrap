95eb4b196880e954307203bff7ade1b8dbdd4060
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-29 14:34:27 +0100

    clean up ssh keys

--- bin/vault.sh
+++ bin/vault.sh
@@ -9,7 +9,7 @@ cd .ssh
 vault2env CICD/global/ssh/automation .env
 source .env
 
-echo "$PRIVAT_KEY_OPENSSH_PEM" | base64 --decode >> automation
+echo "$PRIVAT_KEY_OPENSSH_PEM" | base64 --decode >> automation.openssh.pem
 chmod 600 automation
 echo "$PUBLIC_KEY_SSH" | base64 --decode >> automation.pub
 chmod 600 automation.pub
