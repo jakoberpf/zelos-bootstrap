7b5a5f1480cadf7ad95ff5cedb67e602b8b21166
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-10-30 23:20:51 +0100

    make ssh config from terragrunt

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -42,34 +42,3 @@ resource "local_file" "kubespray_inventory" {
  )
  filename = "${path.root}/../kubespray/inventory.ini"
 }
-
-resource "local_file" "ssh_config" {
-  depends_on = [
-    module.node-jakob.public_ip,
-    module.node-tanja.public_ip,
-    module.node-fabian.public_ip,
-    module.node-ulrike.public_ip,
-    module.node-tobias.public_ip
-  ]
-  content = templatefile("${path.module}/templates/config.tpl",
-  {
-    node-ip = [
-        module.node-jakob.public_ip,
-        module.node-tanja.public_ip,
-        module.node-fabian.public_ip,
-        module.node-ulrike.public_ip,
-        module.node-tobias.public_ip,
-    ]
-    node-id = [
-        "node-jakob",
-        "node-tanja",
-        "node-fabian",
-        "node-ulrike",
-        "node-tobias"
-    ],
-    node-user = "ubuntu",
-    node-key = "${abspath(path.root)}/../.ssh/automation"
-  }
- )
- filename = "${path.root}/../.ssh/config"
-}
