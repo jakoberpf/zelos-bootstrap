4c32e8fcc6153455c6d5d4ae0056cbf4a6d0f2eb
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-03 23:08:25 +0100

    fix formatting issue

--- terraform/templates.tf
+++ terraform/templates.tf
@@ -8,37 +8,37 @@ resource "local_file" "kubespray_inventory" {
     module.node-tobias.public_ip
   ]
   content = templatefile("${path.module}/templates/inventory.tpl",
-  {
-    masters-ip-public = [
+    {
+      masters-ip-public = [
         module.node-jakob.public_ip,
         module.node-tanja.public_ip,
         module.node-fabian.public_ip
-    ]
-    masters-ip-private = [
+      ]
+      masters-ip-private = [
         module.node-jakob.private_ip,
         module.node-tanja.private_ip,
         module.node-fabian.private_ip
-    ]
-    masters-id = [
+      ]
+      masters-id = [
         "node-jakob",
         "node-tanja",
         "node-fabian"
-    ],
-    masters-user = "ubuntu",
-    workers-ip-public = [
+      ],
+      masters-user = "ubuntu",
+      workers-ip-public = [
         module.node-ulrike.public_ip,
         module.node-tobias.public_ip
-    ]
-    workers-ip-private = [
+      ]
+      workers-ip-private = [
         module.node-ulrike.private_ip,
         module.node-tobias.private_ip
-    ]
-    workers-id = [
+      ]
+      workers-id = [
         "node-ulrike",
         "node-tobias"
-    ],
-    workers-user = "ubuntu",
-  }
- )
- filename = "${path.root}/../kubespray/inventory.ini"
+      ],
+      workers-user = "ubuntu",
+    }
+  )
+  filename = "${path.root}/../kubespray/inventory.ini"
 }
