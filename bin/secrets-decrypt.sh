04cb8283d09c3a27932746a3a44ce669cc0e341b
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-04 23:02:44 +0100

    update secrets

--- bin/secrets-decrypt.sh
+++ bin/secrets-decrypt.sh
@@ -4,9 +4,3 @@ cd $GIT_ROOT
 
 sops --decrypt --input-type yaml --output-type yaml terraform/terragrunt.yaml.enc > terraform/terragrunt.yaml
 sops --decrypt terraform/variables.tfvars.enc > terraform/variables.tfvars
-
-sops --decrypt .oci/fabian.pem.enc > .oci/fabian.pem
-sops --decrypt .oci/jakob.pem.enc > .oci/jakob.pem
-sops --decrypt .oci/tanja.pem.enc > .oci/tanja.pem
-sops --decrypt .oci/tobias.pem.enc > .oci/tobias.pem
-sops --decrypt .oci/ulrike.pem.enc > .oci/ulrike.pem
--- bin/secrets-encrypt.sh
+++ bin/secrets-encrypt.sh
@@ -4,9 +4,3 @@ cd $GIT_ROOT
 
 sops --encrypt --input-type yaml --output-type yaml terraform/terragrunt.yaml > terraform/terragrunt.yaml.enc
 sops --encrypt terraform/variables.tfvars > terraform/variables.tfvars.enc
-
-sops --encrypt .oci/fabian.pem > .oci/fabian.pem.enc
-sops --encrypt .oci/jakob.pem > .oci/jakob.pem.enc
-sops --encrypt .oci/tanja.pem > .oci/tanja.pem.enc
-sops --encrypt .oci/tobias.pem > .oci/tobias.pem.enc
-sops --encrypt .oci/ulrike.pem > .oci/ulrike.pem.enc
