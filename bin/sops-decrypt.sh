9f4f0ed71e4d19647a7e18f43214909d25e43283
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-04 18:12:20 +0100

    update secrets

--- bin/sops-decrypt.sh
+++ bin/sops-decrypt.sh
@@ -11,3 +11,6 @@ sops --decrypt .oci/jakob.pem.enc > .oci/jakob.pem
 sops --decrypt .oci/tanja.pem.enc > .oci/tanja.pem
 sops --decrypt .oci/tobias.pem.enc > .oci/tobias.pem
 sops --decrypt .oci/ulrike.pem.enc > .oci/ulrike.pem
+
+sops --encrypt .ssh/automation.enc > .ssh/automation
+sops --encrypt .ssh/automation.pub.enc > .ssh/automation.pub
