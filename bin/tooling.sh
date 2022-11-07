554031e9e24a9be575ba5182f0a35a9895c3dc7d
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-07 21:26:54 +0100

    fix script

--- bin/tooling.sh
+++ bin/tooling.sh
@@ -9,7 +9,7 @@ python -m pip install --upgrade ansible==6.5.0
 ansible-galaxy install caddy_ansible.caddy_ansible,v3.1.0 --force
 
 python -m venv .venv/kubespray
-source .venv/terraform/bin/kubespray
+source .venv/kubespray/bin/activate
 python -m pip install --upgrade pip
 python -m pip install --upgrade netaddr
 python -m pip install --upgrade ansible==2.10.7
