0b8a565f36813358a392166d61264733c01e8898
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2022-11-10 08:40:05 +0100

    cleanup script

--- bin/kubespray-post.sh
+++ bin/kubespray-post.sh
@@ -14,13 +14,11 @@ yq -i '.users[0].name = "admin"' $config_file
 
 # Upload config to vault
 vault_admin_config_base64="$(cat $config_file | base64)"
-vault_last_modified=$(vault kv put CICD/repo/zelos-bootstrap/live/kube-secret admin_conf=$vault_admin_config_base64 | grep "create_time")
-echo "vault: $vault_last_modified"
+vault kv put CICD/repo/zelos-bootstrap/live/kube-secret admin_conf=$vault_admin_config_base64 | grep "create_time"
 
 # Upload config to bucket
 oci_credentials=$(yq e -o=j -I=0 ".oci" kubespray/kubespray.yaml)
 oci_compartment_ocid=$(echo "${oci_credentials}" | yq '.compartment_ocid')
 oci_namespace=$(oci os ns get --profile=KUBESPRAY | jq '.data' | xargs)
 oci_bucket=$(oci os bucket list --profile=KUBESPRAY --compartment-id="$oci_compartment_ocid" | jq '.data[] | select(.name=="zelos")')
-oci_last_modified=$(oci os object put --profile=KUBESPRAY -ns "$oci_namespace" -bn zelos --name kubespray/admin.live.conf --file $config_file --force)
-echo "oci: $oci_last_modified"
+oci os object put --profile=KUBESPRAY -ns "$oci_namespace" -bn zelos --name kubespray/admin.live.conf --file $config_file --force
