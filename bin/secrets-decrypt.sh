00702cc44b7449f2bb416faf8cc430023b1fd624
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-11-05 09:18:09 +0100

    update secrets encrpytion and decryption

--- bin/secrets-decrypt.sh
+++ bin/secrets-decrypt.sh
@@ -2,4 +2,8 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
-sops --decrypt --input-type yaml --output-type yaml terraform/blue/terragrunt.yaml.enc > terraform/blue/terragrunt.yaml
+# Terragrunt / Terraform
+sops --decrypt --input-type yaml --output-type yaml $GIT_ROOT/terraform/$1/terragrunt.yaml.enc > $GIT_ROOT/terraform/$1/terragrunt.yaml
+
+# Ansible
+sops --decrypt --input-type txt --output-type txt $GIT_ROOT/ansible/inventory/$1/.vault_pass.enc > $GIT_ROOT/ansible/inventory/$1/.vault_pass
--- bin/secrets-encrypt.sh
+++ bin/secrets-encrypt.sh
@@ -2,4 +2,8 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
-sops --encrypt --input-type yaml --output-type yaml terraform/blue/terragrunt.yaml > terraform/blue/terragrunt.yaml.enc
+# Terragrunt / Terraform
+sops --encrypt --input-type yaml --output-type yaml $GIT_ROOT/terraform/$1/terragrunt.yaml > $GIT_ROOT/terraform/$1/terragrunt.yaml.enc
+
+# Ansible
+sops --encrypt --input-type txt --output-type txt $GIT_ROOT/ansible/inventory/$1/.vault_pass > $GIT_ROOT/ansible/inventory/$1/.vault_pass.enc
