21d6afa5a6d96a55010f24cbc6db2a01c9f05f54
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-11-05 09:50:50 +0100

    fix ansible workflow

--- bin/ansible-playbook.sh
+++ bin/ansible-playbook.sh
@@ -3,8 +3,10 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 cd $GIT_ROOT
 
 # Source python tooling
-source $GIT_ROOT/.venv/ansible/bin/activate
+# source $GIT_ROOT/.venv/ansible/bin/activate
+
+# Copy generated inventory
+cp terraform/$1/generated/inventory.ini ansible/inventory/$1/inventory.ini
 
 # Run ansible playbook
-cd ansible
-ansible-playbook -T 300 playbook.yml
+ansible-playbook ansible/playbook.yml -i ansible/inventory/$1/inventory.ini
